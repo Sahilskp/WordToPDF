554ed0d9a3e9a87542c8dda1d07383f3fbe00ed7
sahil <sahil.shafeeque@gmail.com>
2020-10-02 16:18:08 +0300

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Office.Interop.Word;
+
+public class Class1
+{
+	public static void main(string[] args)
+	{
+        var appWord = new Application();
+        if (appWord.Documents != null)
+        {
+            //yourDoc is your word document
+            var wordDocument = appWord.Documents.Open("C:\\Education\\3RD EVS Question paper - 28\\Aaliya.docx");
+            string pdfDocName = "C:\\Education\\3RD EVS Question paper - 28\\Aaliya.pdf";
+            if (wordDocument != null)
+            {
+                wordDocument.ExportAsFixedFormat(pdfDocName,
+                WdExportFormat.wdExportFormatPDF);
+                wordDocument.Close();
+            }
+            appWord.Quit();
+        }
+    }
+}
