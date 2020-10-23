843af2fa0a527fd79b2c551e622d99039eac2cf8
sahil <sahil.shafeeque@gmail.com>
2020-10-23 20:35:59 +0300

    first commit

--- Class1.cs
+++ Class1.cs
@@ -1,23 +1,11 @@
 ﻿using System;
-using Microsoft.Office.Interop.Word;
 
 public class Class1
 {
-	public static void main(string[] args)
+	static void main Class1()
 	{
-        var appWord = new Application();
-        if (appWord.Documents != null)
-        {
-            //yourDoc is your word document
-            var wordDocument = appWord.Documents.Open("C:\\Education\\3RD EVS Question paper - 28\\Aaliya.docx");
-            string pdfDocName = "C:\\Education\\3RD EVS Question paper - 28\\Aaliya.pdf";
-            if (wordDocument != null)
-            {
-                wordDocument.ExportAsFixedFormat(pdfDocName,
-                WdExportFormat.wdExportFormatPDF);
-                wordDocument.Close();
-            }
-            appWord.Quit();
-        }
-    }
+		String x = "docx";
+		String a = x.Substring(x.Length - 1);
+		Console.WriteLine(a);
+	}
 }
---
+++ WordToPdf/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Word;
+using Word = Microsoft.Office.Interop.Word;
+using System.IO;
+
+namespace WordToPdf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            * Convert Input.docx into Output.pdf
+            * Please note: You must have the Microsoft Office 2007 Add-in: Microsoft Save as PDF or XPS installed
+            * http://www.microsoft.com/downloads/details.aspx?FamilyId=4D951911-3E7E-4AE6-B059-A2E79ED87041&displaylang=en
+            * Solution source http://cathalscorner.blogspot.com/2009/10/converting-docx-into-doc-pdf-html.html
+            */
+
+            Console.WriteLine("Enter Folder Path: ");
+            String folder = Console.ReadLine();
+            string[] filepaths = Directory.GetFiles(folder);
+
+            Console.WriteLine("Converting Word Documents...");
+            foreach (String file in filepaths) {
+                Convert(file,
+                file.Substring(0,file.Length - 4) + "pdf", WdSaveFormat.wdFormatPDF);
+            }
+
+            Console.WriteLine("Documents... Converted!");
+            Console.ReadKey();
+        }
+
+        // Convert method
+        public static void Convert(string input, string output, WdSaveFormat format)
+        {
+            // Create an instance of Word.exe
+            _Application oWord = new Word.Application
+            {
+
+                // Make this instance of word invisible (Can still see it in the taskmgr).
+                Visible = false
+            };
+            // Interop requires objects.
+            object oMissing = System.Reflection.Missing.Value;
+            object isVisible = true;
+            object readOnly = true;     // Does not cause any word dialog to show up
+            //object readOnly = false;  // Causes a word object dialog to show at the end of the conversion
+            object oInput = input;
+            object oOutput = output;
+            object oFormat = format;
+
+            // Load a document into our instance of word.exe
+            _Document oDoc = oWord.Documents.Open(
+                ref oInput, ref oMissing, ref readOnly, ref oMissing, ref oMissing,
+                ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+                ref oMissing, ref isVisible, ref oMissing, ref oMissing, ref oMissing, ref oMissing
+                );
+
+            // Make this document the active document.
+            oDoc.Activate();
+
+            // Save this document using Word
+            oDoc.SaveAs(ref oOutput, ref oFormat, ref oMissing, ref oMissing,
+                ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+                ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing
+                );
+
+            // Always close Word.exe.
+            oWord.Quit(ref oMissing, ref oMissing, ref oMissing);
+        }
+    }
+}
